2e71b259b9b4c72e61522ac40a8aef4ccde5bd32
Naukri <naukri7707@gmail.com>
2023-07-28 16:33:43 +0800

    新增 CountDownPopup 錄製前倒數計時

---
+++ SignLanguageRecorder/ViewModels/CountDownPopupViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Views;
+using System.Diagnostics;
+
+namespace SignLanguageRecorder.ViewModels;
+
+public partial class CountDownPopupViewModel : ObservableObject
+{
+    public interface IRequirement
+    {
+    }
+
+    private readonly IRequirement requirement;
+
+    [ObservableProperty]
+    public int countDown;
+
+    public event Action OnCompleted = () => { };
+
+    public CountDownPopupViewModel(IRequirement requirement, int seconds)
+    {
+        this.requirement = requirement;
+        CountDown = seconds;
+        _ = StartCountDown();
+    }
+
+    private async Task StartCountDown()
+    {
+        while (CountDown > 0)
+        {
+            await Task.Delay(1000);
+            CountDown--;
+        }
+        OnCompleted.Invoke();
+    }
+}
