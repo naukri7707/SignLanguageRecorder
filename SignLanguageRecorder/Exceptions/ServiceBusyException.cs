06a7bf4def084ac726fcf0078377dafdb46a40aa
Naukri <naukri7707@gmail.com>
2023-07-28 10:19:35 +0800

    新增 ServiceBusyException 處理服務繁忙異常

---
+++ SignLanguageRecorder/Exceptions/ServiceBusyException.cs
@@ -0,0 +1,11 @@
+﻿namespace SignLanguageRecorder.Exceptions;
+
+public class ServiceBusyException : Exception
+{
+    public ServiceBusyException() : base() { }
+
+    public ServiceBusyException(string message) : base(message) { }
+
+    public ServiceBusyException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
