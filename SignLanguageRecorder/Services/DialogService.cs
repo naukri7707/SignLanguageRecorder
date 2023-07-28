ecff1c4982aaa2354290485de8d6449396d4f211
Naukri <naukri7707@gmail.com>
2023-07-28 10:17:19 +0800

    更新 自動顯示 Toast, Snackbar (Unpack win app 不支援)

--- SignLanguageRecorder/Services/DialogService.cs
+++ SignLanguageRecorder/Services/DialogService.cs
@@ -8,57 +8,59 @@ public class DialogService
 
     public Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons)
     {
-        return DialogService.MainPage.DisplayActionSheet(title, cancel, destruction, buttons);
+        return MainPage.DisplayActionSheet(title, cancel, destruction, buttons);
     }
     
     public Task<string> DisplayActionSheet(string title, string cancel, string destruction, FlowDirection flowDirection, params string[] buttons)
     {
-        return DialogService.MainPage.DisplayActionSheet(title, cancel, destruction, flowDirection, buttons);
+        return MainPage.DisplayActionSheet(title, cancel, destruction, flowDirection, buttons);
     }
     
     public Task DisplayAlert(string title, string message, string cancel)
     {
-        return DialogService.MainPage.DisplayAlert(title, message, cancel);
+        return MainPage.DisplayAlert(title, message, cancel);
     }
 
     public Task<bool> DisplayAlert(string title, string message, string accept, string cancel)
     {
-        return DialogService.MainPage.DisplayAlert(title, message, accept, cancel);
+        return MainPage.DisplayAlert(title, message, accept, cancel);
     }
     
     public Task<bool> DisplayAlert(string title, string message, string accept, string cancel, FlowDirection flowDirection)
     {
-        return DialogService.MainPage.DisplayAlert(title, message,accept, cancel, flowDirection);
+        return MainPage.DisplayAlert(title, message,accept, cancel, flowDirection);
     }
     
     public Task DisplayAlert(string title, string message, string cancel, FlowDirection flowDirection)
     {
-        return DialogService.MainPage.DisplayAlert(title , message, cancel, flowDirection);
+        return MainPage.DisplayAlert(title , message, cancel, flowDirection);
     }
 
     public Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = "")
     {
-        return DialogService.MainPage.DisplayPromptAsync(title, message, accept, cancel, placeholder, maxLength, keyboard, initialValue);
+        return MainPage.DisplayPromptAsync(title, message, accept, cancel, placeholder, maxLength, keyboard, initialValue);
     }
 
-    public static IToast Toast(
+    public Task Toast(
         string message,
         ToastDuration duration = ToastDuration.Short,
-        double textSize = AlertDefaults.FontSize
+        double textSize = AlertDefaults.FontSize,
+        CancellationToken token = default
         )
     {
-        return CommunityToolkit.Maui.Alerts.Toast.Make(message, duration, textSize);
+        return CommunityToolkit.Maui.Alerts.Toast.Make(message, duration, textSize).Show(token);
     }
 
-    public static ISnackbar Snackbar(
+    public Task Snackbar(
         string message,
         Action action = null,
         string actionButtonText = AlertDefaults.ActionButtonText,
         TimeSpan? duration = null,
         SnackbarOptions visualOptions = null,
-        IView anchor = null
+        IView anchor = null,
+        CancellationToken token = default
         )
     {
-        return CommunityToolkit.Maui.Alerts.Snackbar.Make(message, action, actionButtonText, duration, visualOptions, anchor);
+        return CommunityToolkit.Maui.Alerts.Snackbar.Make(message, action, actionButtonText, duration, visualOptions, anchor).Show(token);
     }
 }
